2c5db0d6e6305f700a221fffb591d9aaf7bc1b3f
Aya Elymany <aya.elymany2013@gmail.com>
2017-06-17 05:29:32 +0200

    Update 11094 Continents.cpp

--- 11094 Continents/11094 Continents.cpp
+++ 11094 Continents/11094 Continents.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 char g[20][21],l;
-int n,m,vis[20][20];
+int m,n,vis[20][20];
 
 int fill(int x,int y,int flag){
-	if(x==n||x<0)
+	if(x==m||x<0)
 		return 0;
-	if(y<0)y+=m;
-	if(y==m)y=0;
+	if(y<0)y+=n;
+	if(y==n)y=0;
 	if(vis[x][y]!=-1||g[x][y]!=l)
 		return 0;
 	vis[x][y]=flag;
@@ -17,20 +17,20 @@ int fill(int x,int y,int flag){
 
 int main(){
 	int x,y,ans,cnt;
-	while(scanf("%d",&n)==1){
+	while(scanf("%d",&m)==1){
 		cnt=ans=0;
 		memset(vis,-1,sizeof vis);
-		scanf("%d",&m);
-		for(int i=0;i<n;i++){
+		scanf("%d",&n);
+		for(int i=0;i<m;i++){
 			getchar();
-			for(int j=0;j<m;j++)
+			for(int j=0;j<n;j++)
 				g[i][j]=getchar();
 		}
 		scanf("%d%d",&x,&y);
 		l=g[x][y];
 		fill(x,y,cnt++);
-		for(int i=0;i<n;i++)
-			for(int j=0;j<m;j++)
+		for(int i=0;i<m;i++)
+			for(int j=0;j<n;j++)
 				if(g[i][j]==l&&vis[i][j]==-1)
 					ans=max(ans,fill(i,j,cnt++));
 		printf("%d\n",ans);
